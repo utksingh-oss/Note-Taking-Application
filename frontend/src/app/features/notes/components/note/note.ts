e68366761c5cd9f3a986dd05ac5bd3319c729054
utkarsh-singh <utk608@gmail.com>
2026-04-01 07:48:33 +0530

    Removing some components

--- frontend/src/app/features/notes/components/note/note.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-note',
-  imports: [],
-  templateUrl: './note.html',
-  styleUrl: './note.css',
-})
-export class Note {}
